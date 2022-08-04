3595fca3c85628e4f9c78f37ca0eb2bb783dedd5
johelder <johelder.arj@gmail.com>
2022-08-03 23:19:41 -0300

    feat: using header component at app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,16 @@
 import type { AppProps } from 'next/app';
 
+import { Header } from '../components';
+
+import '../styles/global.less';
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Header title="pokexd" />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
