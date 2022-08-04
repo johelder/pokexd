4a098d9336caf0ea1f2f6e72e656f9b878b3527b
johelder <johelder.arj@gmail.com>
2022-08-04 02:07:56 -0300

    feat: create home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,52 @@
-import type { NextPage } from 'next';
-import { Home } from './home';
+import type { GetServerSideProps, NextPage } from 'next';
+import Head from 'next/head';
+import { PokemonTCG } from '../services/pokemontcg';
 
-const App: NextPage = () => {
+import { TCG, TypeButton } from '../components/TypeButton';
+import { Layout } from 'antd';
+
+import styles from './home.module.less';
+
+const { Content: AntdContent } = Layout;
+
+interface IHomeProps {
+  types: TCG[];
+}
+
+const Home = ({ types }: IHomeProps) => {
   return (
     <>
-      <Home />
+      <Head>
+        <title>Home | pokexd</title>
+      </Head>
+
+      <main>
+        <AntdContent className={styles.content}>
+          {types.map(type => (
+            <TypeButton type={type} />
+          ))}
+        </AntdContent>
+      </main>
     </>
   );
 };
 
-export default App;
+export default Home;
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const response = await PokemonTCG.getAllTypes();
+
+  if (!response.ok) {
+    return {
+      notFound: true,
+    };
+  }
+
+  const { data: types } = response.data;
+
+  return {
+    props: {
+      types,
+    },
+  };
+};
