f792d5145dad10bd2f1544d6aa69ceaff1bf422d
johelder <johelder.arj@gmail.com>
2022-08-05 00:30:29 -0300

    refactor: changin TypeButton to use Link next component

--- src/components/TypeButton/index.tsx
+++ src/components/TypeButton/index.tsx
@@ -1,6 +1,6 @@
-import { Button } from 'antd';
+import Link from 'next/link';
 
-import styles from './styles.module.less';
+import { Tag } from 'antd';
 
 export type TCG =
   | 'Colorless'
@@ -20,13 +20,25 @@ interface ITypeButtonProps {
 }
 
 export const TypeButton = ({ type }: ITypeButtonProps) => {
+  const colors = {
+    Colorless: '#e5d6d0',
+    Darkness: '#2c2e2b',
+    Dragon: '#c6a114',
+    Fairy: '#e03a83',
+    Fighting: '#ff501f',
+    Fire: '#e24242',
+    Grass: '#7db808',
+    Lightning: '#fab536',
+    Metal: '#8a776e',
+    Psychic: '#a65e9a',
+    Water: '#5bc7e5',
+  };
+
   return (
-    <Button
-      className={styles[`${type.toLowerCase()}`]}
-      size="large"
-      type="primary"
-    >
-      {type}
-    </Button>
+    <Tag color={colors[type]}>
+      <Link href={`pokemons/${type}`}>
+        <a>{type}</a>
+      </Link>
+    </Tag>
   );
 };
