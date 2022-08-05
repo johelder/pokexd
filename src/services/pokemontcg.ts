cd700ba0e02a14b66b0e65479b588f0b31cff597
johelder <johelder.arj@gmail.com>
2022-08-05 00:31:38 -0300

    feat: create getCardBytype service

--- src/services/pokemontcg.ts
+++ src/services/pokemontcg.ts
@@ -1,12 +1,45 @@
 import { api } from './api';
 
+import { TCG } from '../components/TypeButton';
+import { IPokemon } from '../components/Card';
+
+interface IAllTypesRequest {
+  data: TCG[];
+}
+
+interface ICardsByTypeRequest {
+  data: IPokemon[];
+}
+
 export const PokemonTCG = {
   async getAllTypes() {
     try {
-      const response = await api.get('/types');
+      const response = await api.get<IAllTypesRequest>('/types');
+
+      const { data } = response.data;
 
       return {
-        data: response.data,
+        data,
+        ok: true,
+      };
+    } catch (error) {
+      return {
+        error,
+        ok: false,
+      };
+    }
+  },
+
+  async getCardsByType(type: string) {
+    try {
+      const response = await api.get<ICardsByTypeRequest>(
+        `/cards/?q=types:${type}&pageSize=40`
+      );
+
+      const { data } = response.data;
+
+      return {
+        data,
         ok: true,
       };
     } catch (error) {
