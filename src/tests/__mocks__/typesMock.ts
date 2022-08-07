286ad00f5dd499b123636e86ba3b491d8d4d8d81
johelder <johelder.arj@gmail.com>
2022-08-07 17:17:49 -0300

    test: create types mock

---
+++ src/tests/__mocks__/typesMock.ts
@@ -0,0 +1,15 @@
+import { TCG } from '../../dtos';
+
+export const typesMock: TCG[] = [
+  'Colorless',
+  'Darkness',
+  'Dragon',
+  'Fairy',
+  'Fighting',
+  'Fire',
+  'Grass',
+  'Lightning',
+  'Metal',
+  'Psychic',
+  'Water',
+];
