9d7a43069c508f0bd2c052039aa9313b9f1bb362
johelder <johelder.arj@gmail.com>
2022-08-07 17:07:49 -0300

    refactor: adjust card component

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,58 +1,99 @@
-import Image from 'next/image';
-import { memo } from 'react';
+import { memo, useCallback, useState } from 'react';
 
-import { Card as AntdCard } from 'antd';
+import { IPokemon } from '../../dtos';
 
-const { Meta } = AntdCard;
+import { Modal } from 'antd';
 
 import styles from './styles.module.less';
-
-export interface IPokemon {
-  id: string;
-  images: {
-    small: string;
-    large: string;
-  };
-  subtypes: string[];
-  types: string[];
-  weaknesses: {
-    type: string;
-    value: string;
-  }[];
-  attacks: {
-    name: string;
-    text: string;
-  }[];
-}
 
 interface ICardProps {
   pokemon: IPokemon;
 }
 
 const Card = ({ pokemon }: ICardProps) => {
+  const [isModalVisible, setIsModalVisible] = useState(false);
   const [type] = pokemon.types;
 
+  const handleOpenModal = () => {
+    setIsModalVisible(true);
+  };
+
+  const handleCloseModal = useCallback(() => {
+    setIsModalVisible(false);
+  }, []);
+
   return (
-    <AntdCard
-      hoverable
-      cover={
-        <Image
-          src={pokemon.images.small}
-          alt="Pokemon Trading Card"
-          width="200px"
-          height="250px"
-        />
-      }
-      className={styles[`${type.toLowerCase()}`]}
-    >
-      <Meta
-        title={
-          <div>
-            <span></span>
-          </div>
-        }
-      />
-    </AntdCard>
+    <>
+      <button
+        onClick={handleOpenModal}
+        className={styles[`${type.toLowerCase()}`]}
+        data-testid="pokemon-card"
+      >
+        <div className={styles.imageContainer}>
+          <img
+            src={pokemon.images.small}
+            alt="Pokemon Card Game"
+            data-testid="pokemon-card-image"
+          />
+        </div>
+        <div className={styles.descriptionContainer}>
+          <strong data-testid="pokemon-name">{pokemon.name}</strong>
+
+          {!!pokemon.subtypes && (
+            <span>
+              subtypes:{' '}
+              <p data-testid="pokemon-subtype">
+                {pokemon.subtypes?.join(', ')}
+              </p>
+            </span>
+          )}
+
+          {!!pokemon.weaknesses && (
+            <span>
+              weaknesses:
+              {pokemon.weaknesses?.map(weakness => (
+                <p key={weakness.type} data-testid="pokemon-weakness">
+                  {weakness.type} | {weakness.value}
+                </p>
+              ))}
+            </span>
+          )}
+
+          <span>
+            attacks:
+            {pokemon.attacks?.map((attack, index) => (
+              <p key={index} data-testid="pokemon-attack">
+                {attack.name}
+              </p>
+            ))}
+          </span>
+        </div>
+      </button>
+
+      <Modal
+        visible={isModalVisible}
+        onCancel={handleCloseModal}
+        onOk={handleCloseModal}
+        className={styles.modalContainer}
+        width={650}
+        data-testid="pokemon-modal"
+      >
+        <div className={styles.modalContent}>
+          <img src={pokemon.images.large} alt="Pokemon Card Game" />
+          <article className={styles.modalDescriptionContainer}>
+            {pokemon.attacks?.map((attack, index) => (
+              <>
+                <span key={index}>
+                  {attack.name}
+                  {!!attack.text && ': '}
+                </span>
+                <p>{attack.text}</p>
+              </>
+            ))}
+          </article>
+        </div>
+      </Modal>
+    </>
   );
 };
 
