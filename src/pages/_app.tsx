8d614827202dd30590430a69e2ad07b5367ebef0
johelder <johelder.arj@gmail.com>
2022-08-07 17:11:15 -0300

    refactor: removing header from app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,10 @@
 import type { AppProps } from 'next/app';
 
-import { Header } from '../components';
-
 import '../styles/global.less';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Header title="pokexd" />
       <Component {...pageProps} />
     </>
   );
